2121e93a3633ed93d057c33cdccfdec99efeb9a4
Yahav-N-Hoffman <yahavh@avanan.com>
2021-03-03 14:20:37 +0200

    add baseapi module

---
+++ services/__init__.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+
+class BaseAPI:
+    __HTTP__ = "http://"
+    __HTTPS__ = "https://"
+    __BASE_URL__ = ""
+    __headers__ = {}
+
+    def __init__(self, url):
+        self.__BASE_URL__ = url
+
+    def get(self, url, **kwargs):
+        response = requests.get(url=url, *kwargs)
+        return response
+
+    def digest(self, data):
+        return json.dumps(data)
