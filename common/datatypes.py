e329fc98f143e97131e0ac1e8bcbabdc76d9cb7c
ToxicEnvelope <ynhoffman@gmail.com>
2021-03-06 21:11:40 +0200

    update fingerprint datatype

--- common/datatypes.py
+++ common/datatypes.py
@@ -20,10 +20,21 @@ class Fingerprints(Base):
     longitude = Column(Float(), unique=False, nullable=False)
     location = Column(JSON(), unique=False, nullable=False)
     security = Column(JSON(), unique=False, nullable=False)
+    time_created = Column(Text(), unique=True, nullable=False)
+    request_path = Column(Text(), unique=False, nullable=True)
+    request_params = Column(Text(), unique=False, nullable=True)
+    ssl_cert_chain = Column(JSON(), unique=False, nullable=False)
+    ssl_configuration = Column(JSON(), unique=False, nullable=False)
+    infra_analysis = Column(JSON(), unique=False, nullable=False)
+    malware_check = Column(JSON(), unique=False, nullable=False)
+    connected_domains = Column(JSON(), unique=False, nullable=False)
+    reputation = Column(JSON(), unique=False, nullable=False)
 
     def __init__(self, uid=None, ip=None, hostname=None, city=None, country_code=None, country_name=None, ip_type=None,
                  continent_code=None, continent_name=None, region_code=None, region_name=None, zipcode=None,
-                 latitude=None, longitude=None, location=None, security=None):
+                 latitude=None, longitude=None, location=None, security=None, time_created=None, request_path=None,
+                 request_params=None, ssl_cert_chain=None, ssl_configuration=None, infra_analysis=infra_analysis,
+                 malware_check=None, connected_domains=None, reputation=None):
         self.uid = uid
         self.ip = ip
         self.hostname = hostname
@@ -40,6 +51,15 @@ class Fingerprints(Base):
         self.longitude = longitude
         self.location = location
         self.security = security
+        self.time_created = time_created
+        self.request_path = request_path
+        self.request_params = request_params
+        self.ssl_cert_chain = ssl_cert_chain
+        self.ssl_configuration = ssl_configuration
+        self.infra_analysis = infra_analysis
+        self.malware_check = malware_check
+        self.connected_domains = connected_domains
+        self.reputation = reputation
 
     def __repr__(self):
-        return '<Fingerprint %r>' % self.id
+        return '<Fingerprint %r>' % self.uid
