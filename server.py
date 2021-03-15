1d19cc9189f306771a8226a8d2fc295ef1ffdd77
ToxicEnvelope <ynhoffman@gmail.com>
2021-03-15 10:40:07 +0200

    update server logic

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from datetime import datetime
 
 from enums import StatusType, MediaType
@@ -17,6 +19,8 @@ from services.tip_service import TIPService
 app = FastAPI()
 app.add_middleware(HTTPSRedirectMiddleware)
 app.add_middleware(GZipMiddleware, minimum_size=1000)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.middleware('http')
@@ -25,7 +29,6 @@ async def add_fingerprint_record(request: Request, call_next):
         response: Response = await call_next(request)
 
         ipapi_data = await ipapi_recon(request.client.host)
-
         uid = gen_UUID(request.client.host).__str__()
         ip = ipapi_data['ip'] if 'ip' in ipapi_data else None
         hostname = ipapi_data['hostname'] if 'hostname' in ipapi_data else None
@@ -51,20 +54,22 @@ async def add_fingerprint_record(request: Request, call_next):
         connected_domains = tip_data['connectedDomains']
         reputation = tip_data['reputation']
 
+        req_path = request.url.path
+        req_params = request.url.query
+
         fingerprint = Fingerprints(uid=uid, ip=ip, hostname=hostname, ip_type=ip_type, continent_code=continent_code,
-                                   country_name=continent_name, country_code=country_code, continent_name=country_name,
+                                   country_name=country_name, country_code=country_code, continent_name=continent_name,
                                    region_code=region_code, region_name=region_name, city=city, zipcode=zipcode,
                                    latitude=latitude, longitude=longitude, location=location, security=security,
-                                   time_created=str(datetime.now()), request_path=request.path_params.__str__(),
-                                   request_params=request.query_params.__str__(), infra_analysis=infra_analysis,
+                                   time_created=datetime.now().ctime(), request_path=req_path,
+                                   request_params=req_params, infra_analysis=infra_analysis,
                                    ssl_cert_chain=ssl_cert_chain, ssl_configuration=ssl_configuration,
                                    malware_check=malware_check, connected_domains=connected_domains,
                                    reputation=reputation)
         db_session.add(fingerprint)
         db_session.commit()
-        json = jsonable_encoder(obj=response)
-        return JSONResponse(content=json, media_type=MediaType.APPLICATION_JSON.value, status_code=200)
-    except Exception as e:
+        return response
+    except Exception:
         content = {
             "status": StatusType.FAILED.value,
             "host": request.client.host,
@@ -97,13 +102,17 @@ async def add_recon_headers(request: Request, call_next):
         response.headers["X-RECON-LONGITUDE"] = EncodeHeader(data=fingerprint.longitude).decode()
         response.headers["X-RECON-LOCATION"] = EncodeHeader(data=fingerprint.location).decode()
         response.headers["X-RECON-SECURITY"] = EncodeHeader(data=fingerprint.security).decode()
+        response.headers["X-RECON-INFRA-ANALYSIS"] = EncodeHeader(data=fingerprint.infra_analysis).decode()
+        response.headers["X-RECON-SSL-CERTIFICATION-CHAIN"] = EncodeHeader(data=fingerprint.ssl_cert_chain).decode()
+        response.headers["X-RECON-SSL-CONFIGURATION"] = EncodeHeader(data=fingerprint.ssl_configuration).decode()
+        response.headers["X-RECON-MALWARE-CHECK"] = EncodeHeader(data=fingerprint.malware_check).decode()
+        response.headers["X-RECON-CONNECTED-DOMAINS"] = EncodeHeader(data=fingerprint.connected_domains).decode()
+        response.headers["X-RECON-REPUTATION"] = EncodeHeader(data=fingerprint.reputation).decode()
 
-        response.headers["X-RECON-THREAT-INTELLIGENCE-DATA"] = EncodeHeader(data=tip_data).decode()
-        json = jsonable_encoder(obj=response)
-        return JSONResponse(content=json, media_type=MediaType.APPLICATION_JSON.value, status_code=200)
-    except Exception as e:
+        return response
+    except Exception:
         content = {
-            "status": StatusType.TRACKED.value,
+            "status": StatusType.FAILED.value,
             "host": request.client.host,
             "timestamp": stamp()
         }
@@ -116,9 +125,9 @@ async def ipapi_recon(host, lang='en'):
         data = IPAPIService().check_host(host).with_hostname().with_security().with_language(lang=lang).as_json() \
             .build().preform()
         return data
-    except Exception as e:
+    except Exception:
         content = {
-            "status": StatusType.TRACKED.value,
+            "status": StatusType.FAILED.value,
             "host": host,
             "timestamp": stamp()
         }
@@ -130,9 +139,9 @@ async def tip_recon(domain):
     try:
         data = TIPService().check_domain(domain_name=domain).gather().preform()
         return data
-    except Exception as e:
+    except Exception:
         content = {
-            "status": StatusType.TRACKED.value,
+            "status": StatusType.FAILED.value,
             "host": domain,
             "timestamp": stamp()
         }
@@ -140,26 +149,46 @@ async def tip_recon(domain):
         return JSONResponse(content=json, media_type=MediaType.APPLICATION_JSON.value, status_code=404)
 
 
-@app.get("/")
-def index():
-    content = {"status": None, "timestamp": None, "message": None}
+@app.get("/api/v1/fingerprints")
+async def get_fingerprint(request: Request):
     try:
-        content["status"] = StatusType.SUCCESS.value
-        content["timestamp"] = stamp()
-        content["message"] = "data collected"
-        json = jsonable_encoder(obj=content)
+        fingerprint: Optional[Fingerprints] = Fingerprints.query.filter_by(ip=request.client.host).first()
+        if not fingerprint:
+            content = {
+                "status": StatusType.FAILED.value,
+                "timestamp": stamp()
+            }
+            json = jsonable_encoder(obj=content)
+            return JSONResponse(content=json, media_type=MediaType.APPLICATION_JSON.value, status_code=404)
+        json = jsonable_encoder(obj=fingerprint)
         return JSONResponse(content=json, media_type=MediaType.APPLICATION_JSON.value, status_code=200)
-    except Exception as e:
-        content["status"] = StatusType.TRACKED.value
-        content["timestamp"] = stamp()
-        content["message"] = "nothing happens"
+    except Exception:
+        content = {
+            "status": StatusType.FAILED.value,
+            "timestamp": stamp()
+        }
         json = jsonable_encoder(obj=content)
         return JSONResponse(content=json, media_type=MediaType.APPLICATION_JSON.value, status_code=404)
 
 
+@app.get("/")
+async def index(request: Request):
+    try:
+        return templates.TemplateResponse("gateway.html", {"request": request})
+    except Exception:
+        return templates.TemplateResponse("gateway.html", {"request": request})
+
+
+@app.get("/anonymous/{t}")
+async def task(request: Request, t: str):
+    template = f'{t}.html'
+    return templates.TemplateResponse(template, {"request": request})
+
+
 if __name__ == '__main__':
-    import uvicorn, os
+    import uvicorn
+    import os
     from common import config
-    crt = os.path.join(config.get_root_path(), 'resources', 'localhost.crt')
-    pem = os.path.join(config.get_root_path(), 'resources', 'localhost.key')
+    pem = os.path.join(config.get_root_path(), 'resources', '_.teslathreat.net_private_key.key')
+    crt = os.path.join(config.get_root_path(), 'resources', 'teslathreat.net_ssl_certificate.cer')
     uvicorn.run(app, host="0.0.0.0", port=8443, ssl_certfile=crt, ssl_keyfile=pem)
