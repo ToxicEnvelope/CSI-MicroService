fd6d3c8c6962e7c2a51976659525e197f152b969
SysMurff <ynhoffman@gmail.com>
2022-03-02 00:19:01 +0200

    update tests

--- tests/units/test_module_lambdas.py
+++ tests/units/test_module_lambdas.py
@@ -1,6 +1,6 @@
 import pytest
 from tests import UnitTest
-from lambdas import stamp, __key__, __cipher__, AES, EncodeHeader, DecodeHeader, EncodeAES, DecodeAES
+from lambdas import DateNow, __key__, __cipher__, AES, EncodeAES, DecodeAES
 
 
 class TestModuleLambdas(UnitTest):
@@ -8,26 +8,18 @@ class TestModuleLambdas(UnitTest):
         self.assertEqual(len(__key__), 16, "assert key is not 16 bytes")
 
     def test_cipher_object(self):
-        self.assertEqual(type(__cipher__), type(AES.new(__key__, AES.MODE_CBC)), "assert cipher is not AES object")
+        self.assertEqual(type(__cipher__), type(AES.new(__key__, AES.MODE_ECB)), "assert cipher is not AES object")
 
     def test_EncodeDecode_Headers(self):
         header = {"Authorization": "Bearer 1234567890-"}.__str__()
-        encHeader = EncodeHeader(header)
+        encHeader = EncodeAES(header)
         self.assertNotEqual(encHeader, header, "assert EncodeHeader failed")
-        decHeader = DecodeHeader(encHeader).decode()
+        decHeader = DecodeAES(encHeader).decode()
         self.assertEqual(decHeader, header, "assert DecodeHeader failed")
-
-    ''' TODO : Fix padding error'''
-    # def test_EncodeAES_DecodeAES_text(self):
-    #     payload = "1234567890-"
-    #     ct = EncodeAES(payload)
-    #     print(ct)
-    #     dct = DecodeAES(ct)
-    #     print(dct)
 
     def test_stamp_function(self):
         from time import time
-        self.assertAlmostEqual(stamp(), int(time().__str__()[:10]), "assert stamp function failed")
+        self.assertAlmostEqual(DateNow(), int(time().__str__()[:10]), "assert stamp function failed")
 
 
 if __name__ == '__main__':
