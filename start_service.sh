158ed62097429c7a2795a1775d76e7b7eabe5e79
SysMurff <ynhoffman@gmail.com>
2022-03-02 00:45:28 +0200

    add startup script

--- start_service.sh
+++ start_service.sh
@@ -3,4 +3,4 @@ sudo rm -rf ./nohup.out
 python3 -m venv ./venvCSIMicroService
 ./venvCSIMicroService/bin/activate
 ./venvCSIMicroService/bin/python3 -m pip install -r ./requirements.txt
-nohup ./venvCSIMicroService/bin/python3 ./server.py
+nohup ./venvCSIMicroService/bin/python3 ./server.py &
